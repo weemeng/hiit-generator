6b02a6f20fbff8e010b6ec2eeb50739d5b52b509
Valerie Lui <vallq@users.noreply.github.com>
2020-02-22 15:54:52 +0800

    Modified Home.js to not be a class

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,55 @@
 import React from "react";
-import Header from "./Header";
-import Profile from "./Profile";
-import Summary from "./Summary";
 import { Link } from "react-router-dom";
+import Header from "./Header";
+import "./Home.css";
+
+const HiitMeUp = () => {
+  return (
+    <div className="home" data-testid="home">
+      <div>
+        <Link to="/set-workout">
+          <button className="hitt-me">HIIT ME UP</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="about">
+      {<p>
+        HIIT ME UP is a casual web app to make getting that next workout done effortlessly &#9786; Please feel free to send your feedback for improvements or other features <a href="mailto:vallq@outlook.com">here</a>!
+      </p>}
+    </div>
+  );
+};
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isDisplayed: true
+      displayAbout: false
     };
   }
 
+  toggleAbout = event => {
+    this.setState(prevState => {
+      const displayAbout = prevState.displayAbout;
+      return {displayAbout: !displayAbout};
+    });
+  };
+
   render() {
+    // let {displayAbout} = this.state;
+    // return <div>{displayAbout ? <About /> : <HiitMeUp />}</div>;
     return (
-      <div data-testid="home">
+      <div>
         <Header />
-        <Profile />
-        <Summary />
-        <div>
-          <Link to="/set-workout">
-            <button>New Workout</button>
-          </Link>
+        <div className="home-box">
+          {!this.state.displayAbout ? (HiitMeUp()) : (About())}
         </div>
+        <button onClick={this.toggleAbout}>{!this.state.displayAbout ? ("About") : ("Go Back")}</button>
       </div>
     );
   }
