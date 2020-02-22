0ba3d9d9b6f0d103899cbc2d77fb46fbd1f7dcbd
Valerie Lui <vallq@users.noreply.github.com>
2020-02-22 15:53:56 +0800

    Removed className from Hheader tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import "../App.css";
 
 function Header() {
   return (
-    <header className="header">
-      <h1>HIIT ME UP</h1>
-      <hr></hr>
-    </header>
+      <header>
+        <h1>HIIT ME UP</h1>
+        <hr></hr>
+      </header>
   );
 }
 
