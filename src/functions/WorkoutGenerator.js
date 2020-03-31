243b1182e590804c072022398953453836a163fd
Valerie Lui <vallq@users.noreply.github.com>
2020-03-31 21:23:33 +0800

    add 30s for interval

--- src/functions/WorkoutGenerator.js
+++ src/functions/WorkoutGenerator.js
@@ -63,14 +63,14 @@ class WorkoutGenerator extends React.Component {
     return setInterval(() => {
       let currIdx = this.state.nextIndex;
       let nextIdx = this.state.nextIndex + 1;
-      if (currIdx === this.state.exerciseArray.length-1) {
+      if (currIdx === this.state.exerciseArray.length - 1) {
         nextIdx = 0;
       }
       this.setState({
         currentIndex: currIdx,
         nextIndex: nextIdx
       });
-    });
+    }, 30000);
   };
 
   componentDidMount() {
@@ -96,7 +96,8 @@ class WorkoutGenerator extends React.Component {
             </span>
           </div>
           <h3>
-            Current Exercise: <br></br> {this.state.exerciseArray[this.state.currentIndex]}
+            Current Exercise: <br></br>{" "}
+            {this.state.exerciseArray[this.state.currentIndex]}
           </h3>
         </div>
         <div className="bottom-row">
@@ -112,7 +113,8 @@ class WorkoutGenerator extends React.Component {
           </div>
           <div className="next-exercise-render">
             <h5>
-              Next Exercise: <br></br> {this.state.exerciseArray[this.state.nextIndex]}
+              Next Exercise: <br></br>{" "}
+              {this.state.exerciseArray[this.state.nextIndex]}
             </h5>
           </div>
         </div>
