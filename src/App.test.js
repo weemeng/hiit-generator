3d71dd02ff22251ebb13475a6f9668090c25e6d6
Valerie Lui <vallq@users.noreply.github.com>
2020-04-01 21:40:16 +0800

    Add test for App to render Home component

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  it("should render Home when initialised", () => {
-    const { getByTestId } = render(<App />);
-    const homeComponent = getByTestId("home");
-    expect(homeComponent).toBeInTheDocument();
+  test("<Home> should render", () => {
+    const { getByLabelText } = render(<App />);
+    const HomeComponent = getByLabelText("home");
+    expect(HomeComponent).toBeInTheDocument();
   });
 });
